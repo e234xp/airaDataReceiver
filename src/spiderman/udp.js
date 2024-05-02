a8384bbf4eb2630b7d485f7c1bcf62baa17ea48c
elvis.huang <elvis.huang@aira.com.tw>
2024-05-02 13:14:36 +0800

    fix: 調大 udp 發送時的 buffer size

--- src/spiderman/udp.js
+++ src/spiderman/udp.js
@@ -3,7 +3,9 @@ const dgram = require("dgram");
 module.exports = () => {
   function create() {
     const client = dgram.createSocket("udp4");
-
+    client.on("connect", () => {
+      client.setSendBufferSize(65535)
+    })
     return client;
   }
 
